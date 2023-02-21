d5ed13c447e25f79c1fe51fd19deb8635dc672b7
Hamza11hk <Hamzahk110077@gmail.com>
2023-02-21 13:23:12 -0800

    answer of tasks 0,1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return:  0
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		putchar(i);
+		putchar('\n';
+	}
+}
