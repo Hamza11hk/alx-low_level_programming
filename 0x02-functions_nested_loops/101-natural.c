7bc52961fa03585ef2c2785d71dc2fd9d42a9a36
Hamza11hk <Hamzahk110077@gmail.com>
2023-02-21 18:01:02 -0800

    advanced tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+
+	}
+	printf("%d\n", j);
+	return (0);
+}
