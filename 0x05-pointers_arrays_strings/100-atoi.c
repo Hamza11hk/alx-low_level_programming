f15e25ce389ffe2a9909de46aad15c426f484516
Hamza11hk <Hamzahk110077@gmail.com>
2023-02-27 15:53:57 -0800

    rewrite task 11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,27 @@
 #include "main.h"
-#include "2-strlen.c"
+
 /**
- * _atoi - converts string to integer
- * @s: string to convert
- * Return: returns integer value
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-	int a;
-	int b = 0;
-	int c;
-	int d = 1;
-	int n = 0;
+	int g = 1;
+	unsigned int n = 0;
 
-	for (a = 0; a < _strlen(s); a++)
-	{
-		if (!(s[a] >= '0' && s[a] <= '9') && c > 0)
+	do {
+		if (*s == '-')
+			g *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
 			break;
-		if (s[a] == '-')
-			b--;
-		if (s[a] == '+')
-			b++;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			c++;
-		}
-	}
-	while (c > 0)
-	{
-		n += ((s[a - 1] - '0') * d);
-		a--;
-		c--;
-		d *= 10;
-	}
-	if (b >= 0)
-	{
-		n *= 1;
-	} else
-	{
-		n *= -1;
-	}
-	return (n);
+
+	} while (*s++);
+
+	return (n * g);
 }
