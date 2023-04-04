02d96d7d20861469e453615a2ceb041168e26043
Hamza11hk <Hamzahk110077@gmail.com>
2023-04-04 16:28:23 -0700

    fix task 10 by using While loop instead of For loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,6 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *pv = NULL , *ct = *head;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 
@@ -21,10 +22,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (unsigned int i = 0; i < index && ct != NULL; i++)
+	while (i < index && ct != NULL)
+
 	{
 		pv = ct;
 		ct = ct->next;
+		i++;
 	}
 
 	if (pv == NULL || ct == NULL)
